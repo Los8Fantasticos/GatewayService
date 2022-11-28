5093ce2f4979bce3893fbd9e2327d67d0800ecd3
Gonzalo_Sarsy <gonzalo.ibanez@sarsy-srl.com>
2022-11-28 15:38:56 -0300

    Finaliza la aplicación

--- ApiGateway/Api/Configurations/LoggerConfig.cs
+++ ApiGateway/Api/Configurations/LoggerConfig.cs
@@ -1,4 +1,8 @@
-﻿namespace Api.Configurations
+﻿using Serilog;
+using Serilog.Filters;
+using System.Reflection;
+
+namespace Api.Configurations
 {
     public static class LoggerConfig
     {
@@ -7,22 +11,23 @@
         {
             #region Logging
 
-            //_ = builder.Host.UseSerilog((hostContext, loggerConfiguration) =>
-            //{
-            //    var assembly = Assembly.GetEntryAssembly();
-            //    _ = loggerConfiguration.WriteTo.MSSqlServer(connectionString: builder.Configuration.GetConnectionString("SqlConnection"),
-            //        tableName: builder.Configuration.GetSection("Serilog").GetSection("TableName").Value,
-            //        appConfiguration: builder.Configuration,
-            //        autoCreateSqlTable: true,
-            //        columnOptionsSection: builder.Configuration.GetSection("Serilog").GetSection("ColumnOptions"),
-            //        schemaName: builder.Configuration.GetSection("Serilog").GetSection("SchemaName").Value);
-            //    _ = loggerConfiguration.WriteTo.MSSqlServer(connectionString: builder.Configuration.GetConnectionString("SqlConnection2"),
-            //        tableName: builder.Configuration.GetSection("Serilog").GetSection("TableName").Value,
-            //        appConfiguration: builder.Configuration,
-            //        autoCreateSqlTable: true,
-            //        columnOptionsSection: builder.Configuration.GetSection("Serilog").GetSection("ColumnOptions"),
-            //        schemaName: builder.Configuration.GetSection("Serilog").GetSection("SchemaName").Value);
-            //});
+            bool EnableMicrosoftLogs = builder.Configuration.GetValue<bool>("Serilog:EnableMicrosoftLogs");
+            bool EnableMicrosoftAspNetCoreLogs = builder.Configuration.GetValue<bool>("Serilog:EnableMicrosoftAspNetCoreLogs");
+            bool EnableSystemLogs = builder.Configuration.GetValue<bool>("Serilog:EnableSystemLogs");
+
+            _ = builder.Host.UseSerilog((hostContext, loggerConfiguration) =>
+            {
+                if (!EnableMicrosoftLogs) loggerConfiguration.Filter.ByExcluding(Matching.FromSource("Microsoft"));
+                if (!EnableSystemLogs) loggerConfiguration.Filter.ByExcluding(Matching.FromSource("System"));
+                if (!EnableMicrosoftAspNetCoreLogs) loggerConfiguration.Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore"));
+                var assembly = Assembly.GetEntryAssembly();
+                _ = loggerConfiguration.WriteTo.MSSqlServer(connectionString: builder.Configuration.GetConnectionString("SqlConnection"),
+                    tableName: builder.Configuration.GetSection("Serilog").GetSection("TableName").Value,
+                    appConfiguration: builder.Configuration,
+                    autoCreateSqlTable: true,
+                    columnOptionsSection: builder.Configuration.GetSection("Serilog").GetSection("ColumnOptions"),
+                    schemaName: builder.Configuration.GetSection("Serilog").GetSection("SchemaName").Value);
+            });
 
             #endregion Logging
 
